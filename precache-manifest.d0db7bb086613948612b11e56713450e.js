cdd42d913d48ad7c246c92ac1ad92cdedfc3dbc0
Vahan Karamyan <karamyanvahan@gmail.com>
2020-08-25 21:41:06 +0400

    Updates

---
+++ precache-manifest.d0db7bb086613948612b11e56713450e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb29d9bbe5f83862ea778b67eeb0bf9",
+    "url": "/%7BGithub-repo-name%7D/index.html"
+  },
+  {
+    "revision": "d3838d096c6365c0a308",
+    "url": "/%7BGithub-repo-name%7D/static/css/main.9da6f456.chunk.css"
+  },
+  {
+    "revision": "74b733fea058a0000391",
+    "url": "/%7BGithub-repo-name%7D/static/js/2.6a60cda3.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/%7BGithub-repo-name%7D/static/js/2.6a60cda3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3838d096c6365c0a308",
+    "url": "/%7BGithub-repo-name%7D/static/js/main.13b6ff61.chunk.js"
+  },
+  {
+    "revision": "089c6cfcb8edb7a93d9b",
+    "url": "/%7BGithub-repo-name%7D/static/js/runtime-main.1d131803.js"
+  }
+]);
